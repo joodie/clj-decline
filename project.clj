6f43eb0f418625eb0c8c3a60aace8271bb7623c4
Joost Diepenmaat <joost@zeekat.nl>
2011-03-12 21:07:43 +0100

    prepare v0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-decline "0.0.3"
+(defproject clj-decline "0.0.4"
   :description "Minimal, flexible validation library - test objects and collect reasons for rejection"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
