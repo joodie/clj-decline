fc86c8054f9d9b094b8775fec729f3c2ed7ae397
Joost Diepenmaat <joost@zeekat.nl>
2011-03-21 19:55:52 +0100

    bump version to 0.0.5

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clj-decline "0.0.4"
+(defproject clj-decline "0.0.5"
   :description "Minimal, flexible validation library - test objects and collect reasons for rejection"
   :dependencies [[org.clojure/clojure "1.2.0"]])
