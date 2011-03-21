47d12cb498c5b6e62c2023b079e6ddd8da1ab495
Joost Diepenmaat <joost@zeekat.nl>
2011-03-21 19:54:50 +0100

    remove unused dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject clj-decline "0.0.4"
   :description "Minimal, flexible validation library - test objects and collect reasons for rejection"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]])
